939bfdf0b4eadbac04ad6182d0c4630a7d50bf98
Vladimir Pilipenko <pilip2004@gmail.com>
2022-10-13 12:25:13 +0300

    Add Examp005

---
+++ Еxample005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write (“Введите имя пользователя: ”);
+string username = Console.ReadLine();
+
+if(username == “Катя”)
+{
+    Сonsole.WriteLine(Ура, это же КАТЯ);
+}
+else
+{
+Сonsole.Write("Привет, ");
+Сonsole.WriteLine(username);
+}
